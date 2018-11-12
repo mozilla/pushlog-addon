99f38e68eb6657b5f55e23c917b6759a54bc3a80
philipp <git.99.antijunk@spamgourmet.com>
2018-11-12 16:36:04 +0100

    Update buglist-link.js
    incorporate points raised in review

--- buglist-link.js
+++ buglist-link.js
@@ -1,37 +1,37 @@
-var bugs = [];
-
 function createBuglist() {
+	const bugs = new Set();
+	const titlebar = document.querySelector('div.title');
+	
+	if (!titlebar) {
+		return
+	}
+	titlebar.style = 'overflow:auto';
+
 	//look for Bug # on page
 	let result;
 	let re = /Bug ([0-9]+)/ig;
 	while ((result = re.exec(document.body.innerText)) !== null) {
-		bugs.push(result[1]);
+		bugs.add(result[1]);
 	}
-	//consolidate Bug #
-	bugs = new Set(bugs);
 
 	if (bugs.size === 0) {
 		return;
 	}
 
-	var title = document.getElementsByClassName('title');
-	var titlebar = title[0];
-	titlebar.style = 'overflow:auto';
+	//generate url for buglist link
+	let url = 'https://bugzilla.mozilla.org/buglist.cgi?bug_id=';
+	bugs.forEach(bug => {
+		url += bug + ',';
+	});
 
+	//create buglist link
 	let a = document.createElement('a');
 	a.textContent = 'View as Buglist on Bugzilla';
-	a.href = 'https://bugzilla.mozilla.org/buglist.cgi?bug_id=';
+	a.href = url;
 	a.id = 'addon-buglist';
 	a.style = 'float:right';
 
-	//create generic buglist-link
 	titlebar.appendChild(a);
-
-	//fill link to buglist with data
-	for (let bug of bugs) {
-		let a = document.getElementById('addon-buglist');
-		a.href += bug + ",";
-	}
 }
 
 createBuglist();
