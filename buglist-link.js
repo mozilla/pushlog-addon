51fbd92ee6cd3c41623394d29d82e6cfbbd99389
philipp <philipp-sumo@users.noreply.github.com>
2019-01-16 19:50:27 +0100

    enhance hg pushlog with link to buglist on bugzilla  (#3)
    * enhance hg pushlog with link to buglist on bugzilla
    
    * Update buglist-link.js
    
    * Update buglist-link.js
    
    incorporate points raised in review
    
    * better match pattern

---
+++ buglist-link.js
@@ -0,0 +1,37 @@
+function createBuglist() {
+	const bugs = new Set();
+	const titlebar = document.querySelector('div.title');
+	
+	if (!titlebar) {
+		return
+	}
+	titlebar.style = 'overflow:auto';
+
+	//look for Bug # on page
+	let result;
+	let re = /Bug ([0-9]+)/ig;
+	while ((result = re.exec(document.body.innerText)) !== null) {
+		bugs.add(result[1]);
+	}
+
+	if (bugs.size === 0) {
+		return;
+	}
+
+	//generate url for buglist link
+	let url = 'https://bugzilla.mozilla.org/buglist.cgi?bug_id=';
+	bugs.forEach(bug => {
+		url += bug + ',';
+	});
+
+	//create buglist link
+	let a = document.createElement('a');
+	a.textContent = 'View as buglist on Bugzilla';
+	a.href = url;
+	a.id = 'addon-buglist';
+	a.style = 'float:right';
+
+	titlebar.appendChild(a);
+}
+
+createBuglist();
